ec0f62cdee8d1aeb92bb1e2f831d4bc0b1aba4bc
Yazan70 <titiyazan14@gmail.com>
2023-06-29 05:32:44 +0300

    update

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Fun_two (){
+    return(
+        <div>
+            <h1>i am footer</h1>
+            <p>react function</p>
+        </div>
+    );
+
+}
+export default Fun_two
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function S() {
+    return (
+        <div className="parent1">
+            <h1>hello i am header</h1>
+            <p>yaa</p>
+            <input></input>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default S;
